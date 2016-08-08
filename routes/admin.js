ddb7bbdb283148505eba5979479351e8197374ea
nodejh <jianghangscu@gmail.com>
2016-08-08 16:44:24 +0800

    用户文件查看

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,16 @@
 var Weekly = require('../models/weekly.model');
 var Admin = require('../models/admin.model');
+var Folder = require('../models/folder.model');
+var File = require('../models/file.model');
 var routes = function (app) {
 
 
 
   app.get('/admin', function (req, res) {
-    res.render('admin_index', {
-      title: '管理员后台'
-    });
+    res.redirect('/admin/users');
+    // res.render('admin_index', {
+    //   title: '管理员后台'
+    // });
   });
 
 
@@ -54,6 +57,93 @@ var routes = function (app) {
       });
     });
   });
+
+
+
+  app.get('/admin/folder', function (req, res) {
+
+    Admin.getAllFolder(function (err, rows) {
+      if (err) {
+       console.log('error 查看所有文件夹失败!', err);
+        return next(err);
+      }
+      // Admin.getUser();
+      res.render('admin_folder', {
+        title: '所有文件夹',
+        error: '',
+        success: '',
+        user: {
+          name: '管理员'
+        },
+        folders: rows
+      });
+    });
+  });
+
+
+  // 查询所有用户
+  app.get('/admin/users', function (req, res) {
+    Admin.getUsers(function (err, rows) {
+
+      if (err) {
+        return next(err);
+      }
+      res.render('admin_users', {
+        title: '所有用户',
+        error: '',
+        success: '',
+        user: {
+          name: '管理员'
+        },
+        users: rows
+      });
+    });
+  });
+
+
+
+
+
+  // API
+  app.post('/api/admin/user_folder', function (req, res) {
+    var user_id = req.body.user_id;
+    Folder.findFoldersByUserId(user_id, function (err, rows) {
+      if (err) {
+        console.log('error select folder: ', err);
+        return res.json({
+          code: 1,
+          msg: err
+        });
+      }
+      res.json({
+        code: 0,
+        data: {
+          folders: rows
+        }
+      });
+    });
+  });
+
+  app.post('/api/admin/files_in_folder', function (req, res) {
+    var folder_id = req.body.folder_id;
+    File.findFilesByFolderId(folder_id, function (err, rows) {
+      if (err) {
+        console.log('error select file: ', err);
+        return res.json({
+          code: 1,
+          msg: err
+        });
+      }
+      res.json({
+        code: 0,
+        data: {
+          files: rows
+        }
+      });
+    });
+  });
+
+
 };
 
 module.exports = routes;
